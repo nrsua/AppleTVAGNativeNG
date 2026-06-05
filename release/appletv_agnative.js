1b880106144ff3fcf1e1e8dc7726a97b3c75b62e
nrsua <277812736+nrsua@users.noreply.github.com>
2026-06-05 20:22:28 +0300

    bump minor version to 0.4.2

--- release/appletv_agnative.js
+++ release/appletv_agnative.js
@@ -76,7 +76,7 @@
     SETTINGS_HIDE_COMPONENT: 'agnative_settings_hide'
   };
 
-  const PLUGIN_VERSION = '0.4.1';
+  const PLUGIN_VERSION = '0.4.2';
   const PLUGIN_AUTHORS = 'llowmikee, nrsua, gwynnbleiidd, arabianq, ang3el7z, dimir96';
 
   const ru = {
